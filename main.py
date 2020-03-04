4a85376d482ad15242f76e0c695ad3f849cddde9
Rohit Chaudhary <rohitchaudharylochab@gmail.com>
2020-03-04 20:24:39 +0530

    First Change

---
+++ main.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
+import json
+from datetime import datetime
+
+with open('config.json', 'r') as c:
+    params = json.load(c)["params"]
+
+local_server = True
+app = Flask(__name__)
+app.config.update(
+    MAIL_SERVER = 'smpt.gmail.com',
+    MAIL_PORT = '465'
+    MAIL_USE_SSL =True,
+    MAIL_USERNAME = params['gmail_user'],
+    MAIL_PASSWORD = params['gmail_pasword']
+)
+mail = Mail(app)
+
+
+if(local_server):
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
+
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_uri']
+
+db =SQLAlchemy(app)
+
+class Contacts(db.Model):
+    sno = db.Column(db.Integer, primary_key=True) 
+    name = db.Column(db.String(20),nullable=True) 
+    phone_num = db.Column(db.Integer) 
+    msg = db.Column(db.String(12),nullable=True)
+    date = db.Column(db.String(12),nullable=True) 
+    email = db.Column(db.String(200),nullable=True) 
+ 
+
+@app.route("/")
+def home():
+    return render_template("index.html", params = params)
+    
+@app.route("/about")
+def about():
+    return render_template("about.html", params = params)
+
+@app.route("/contact", methods = ['GET', 'POST'])
+def contact():
+    if(request.method== 'POST'):
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        message = request.form.get('message')
+        entry = Contacts(name=name, phone_num=phone, msg=message, date=datetime.now(), email=email)
+        db.session.add(entry)
+        db.session.commit()
+        mail.send_message('New message from' + name,
+        sender=email,
+        recipents=[param[gmail_user]],)
+        body = message + "\n" + phone
+    return render_template("contact.html", params = params)
+
+@app.route("/post")
+def post():
+    return render_template("post.html", params = params)
+    
+app.run(debug=True)
+
+ 
